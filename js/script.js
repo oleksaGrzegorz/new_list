4f9c7fed7827b91a66bfeaea1e6403c26528dda8
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-12-25 17:05:06 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,21 +10,6 @@
     },
   ];
 
-  const render = () => {
-    htmlString = "";
-    for (const task of tasks) {
-      htmlString += `
-        <li 
-            ${task.done ? ' style="text-decoration: line-through"' : ""}
-        > 
-            ${task.content}
-        </li>
-      `;
-    }
-
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
-
   const addNewTask = (newTaskContent) => {
     tasks.push({
       content: newTaskContent,
@@ -33,18 +18,45 @@
     render();
   };
 
+  const render = () => {
+    htmlString = "";
+    for (const task of tasks) {
+      htmlString += `
+        <li 
+            ${task.done ? ' style="text-decoration: line-through"' : ""}
+        > 
+        <button class="js-remove">usun</button>
+            ${task.content}
+        </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButtons, index) => {
+      removeButtons.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
   const onFormSumit = (event) => {
-      event.preventDefault();
+    event.preventDefault();
 
-      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-      if (newTaskContent === "") {
-        return;
-      }
-      addNewTask(newTaskContent);
-    };
-
-
+    if (newTaskContent === "") {
+      return;
+    }
+    addNewTask(newTaskContent);
+  };
 
   const init = () => {
     render();
